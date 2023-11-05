9d37fa8faf6de1c286ecb2e69bb9fec633064781
DeIT <danarikram@gmail.com>
2023-11-05 23:44:42 +0700

    using sepolia network

--- context/constants.js
+++ context/constants.js
@@ -1,4 +1,4 @@
 import market from './NFTMarketplace.json';
 
-export const MarketAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const MarketAddress = '0x5675a1B5364a8Be495A1Fbb84500EA1d57360b34';
 export const MarketAddressABI = market.abi;
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,15 @@
 require('@nomiclabs/hardhat-waffle');
+// Load environment variables from .env
+require('dotenv').config();
+
+const { SEPOLIA_API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: '0.8.4',
+  networks: {
+    sepolia: {
+      url: SEPOLIA_API_URL, // Infura or Alchemy URL for Goerli
+      accounts: [`0x${PRIVATE_KEY}`], // Private key of the deployer account
+    },
+  },
 };
